cafa8f37e900158571ff0cb9f2c83f7a549a8143
Eric Lief <liefe@u-pl10.ms.mff.cuni.cz>
2018-02-25 20:40:32 +0100

    changed sim

---
+++ src/runsim.sh
@@ -0,0 +1,35 @@
+javac -d ../bin *.java
+gcc cachesim.c -o cachesim
+
+# Cache sim tests
+
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 64 64
+
+
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 64 64 >> ../output/"sim-obliv64-64.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 64 1024 >> ../output/"sim-obliv64-1024.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 64 4096 >> ../output/"sim-obliv64-4096.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 512 512 >> ../output/"sim-obliv512-512.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimCO | ./cachesim 4096 64 >> ../output/"sim-obliv4096-64.csv"
+
+
+# ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimNaive | ./cachesim 64 64 >> ../output/"sim-naive64-64.csv"
+ #ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimNaive | ./cachesim 64 1024 >> ../output/"sim-naive64-1024.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimNaive | ./cachesim 64 4096 >> ../output/"sim-naive64-4096.csv"
+#ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimNaive | ./cachesim 512 512 >> ../output/"sim-naive512-512.csv"
+ulimit -t unlimited && nice -n 19 java -cp ../bin TestMatrixSimNaive | ./cachesim 4096 64 >> ../output/"sim-naive4096-64.csv"
+
+
+
+#java Matrix 64 64 | ./cachesim 64 64 >> ../output/"cachesim-obliv64-64.csv"
+#java Matrix 64 1024 | ./cachesim 64 1024 >> ../output/"cachesim-obliv64-1024.csv"
+#java Matrix 64 4096 | ./cachesim 64 4096 >> ../output/"cachesim-obliv64-4096.csv"
+#java Matrix 512 512 | ./cachesim 512 512 >> ../output/"cachesim-obliv512-512.csv"
+#java Matrix 4096 64 | ./cachesim 4096 64 >> ../output/"cachesim-obliv4096-64.csv"
+
+# Normal tests
+# java -cp ../bin Matrix 64 64 
+# java -cp ../bin Matrix 64 1024
+# java -cp ../bin Matrix 64 4096
+# java -cp ../bin Matrix 512 512
+# java -cp ../bin Matrix 4096 64
